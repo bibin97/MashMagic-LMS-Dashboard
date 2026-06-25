02fb7e0a4409892b364093cd1ad14e06f899c23a
bibin <bibinthankachan29@gmail.com>
2026-06-25 12:00:55 +0530

    Broaden LIKE clauses in fix_times to handle leading spaces or different time formats

--- backend/fix_times.js
+++ backend/fix_times.js
@@ -15,22 +15,22 @@ async function fixTimes() {
         console.log("Connected to DB. Fixing times...");
 
         // Devadarsh: 7:30 PM (19:30:00)
-        await db.query(`UPDATE timetable t JOIN students s ON t.student_id = s.id SET t.start_time = '19:30:00', t.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND t.start_time LIKE '10:00:%'`);
-        await db.query(`UPDATE faculty_schedules fs JOIN students s ON fs.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND fs.start_time LIKE '10:00:%'`);
-        await db.query(`UPDATE faculty_sessions fs JOIN timetable t ON fs.timetable_id = t.id JOIN students s ON t.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND fs.start_time LIKE '10:00:%'`);
-        await db.query(`UPDATE faculty_sessions fs JOIN session_attendance sa ON fs.id = sa.session_id JOIN students s ON sa.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND fs.start_time LIKE '10:00:%'`);
+        await db.query(`UPDATE timetable t JOIN students s ON t.student_id = s.id SET t.start_time = '19:30:00', t.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND (t.start_time LIKE '%10:00%' OR t.start_time LIKE '%10:%')`);
+        await db.query(`UPDATE faculty_schedules fs JOIN students s ON fs.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND (fs.start_time LIKE '%10:00%' OR fs.start_time LIKE '%10:%')`);
+        await db.query(`UPDATE faculty_sessions fs JOIN timetable t ON fs.timetable_id = t.id JOIN students s ON t.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND (fs.start_time LIKE '%10:00%' OR fs.start_time LIKE '%10:%')`);
+        await db.query(`UPDATE faculty_sessions fs JOIN session_attendance sa ON fs.id = sa.session_id JOIN students s ON sa.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%devadarsh%' AND (fs.start_time LIKE '%10:00%' OR fs.start_time LIKE '%10:%')`);
 
         // Ryan: 7:30 PM (19:30:00)
-        await db.query(`UPDATE timetable t JOIN students s ON t.student_id = s.id SET t.start_time = '19:30:00', t.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND t.start_time LIKE '07:30:%'`);
-        await db.query(`UPDATE faculty_schedules fs JOIN students s ON fs.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND fs.start_time LIKE '07:30:%'`);
-        await db.query(`UPDATE faculty_sessions fs JOIN timetable t ON fs.timetable_id = t.id JOIN students s ON t.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND fs.start_time LIKE '07:30:%'`);
-        await db.query(`UPDATE faculty_sessions fs JOIN session_attendance sa ON fs.id = sa.session_id JOIN students s ON sa.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND fs.start_time LIKE '07:30:%'`);
+        await db.query(`UPDATE timetable t JOIN students s ON t.student_id = s.id SET t.start_time = '19:30:00', t.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND (t.start_time LIKE '%07:30%' OR t.start_time LIKE '%7:30%')`);
+        await db.query(`UPDATE faculty_schedules fs JOIN students s ON fs.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND (fs.start_time LIKE '%07:30%' OR fs.start_time LIKE '%7:30%')`);
+        await db.query(`UPDATE faculty_sessions fs JOIN timetable t ON fs.timetable_id = t.id JOIN students s ON t.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND (fs.start_time LIKE '%07:30%' OR fs.start_time LIKE '%7:30%')`);
+        await db.query(`UPDATE faculty_sessions fs JOIN session_attendance sa ON fs.id = sa.session_id JOIN students s ON sa.student_id = s.id SET fs.start_time = '19:30:00', fs.end_time = '20:30:00' WHERE s.name LIKE '%ryan%' AND (fs.start_time LIKE '%07:30%' OR fs.start_time LIKE '%7:30%')`);
 
         // Zenha: 1:00 PM (13:00:00)
-        await db.query(`UPDATE timetable t JOIN students s ON t.student_id = s.id SET t.start_time = '13:00:00', t.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND t.start_time LIKE '01:00:%'`);
-        await db.query(`UPDATE faculty_schedules fs JOIN students s ON fs.student_id = s.id SET fs.start_time = '13:00:00', fs.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND fs.start_time LIKE '01:00:%'`);
-        await db.query(`UPDATE faculty_sessions fs JOIN timetable t ON fs.timetable_id = t.id JOIN students s ON t.student_id = s.id SET fs.start_time = '13:00:00', fs.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND fs.start_time LIKE '01:00:%'`);
-        await db.query(`UPDATE faculty_sessions fs JOIN session_attendance sa ON fs.id = sa.session_id JOIN students s ON sa.student_id = s.id SET fs.start_time = '13:00:00', fs.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND fs.start_time LIKE '01:00:%'`);
+        await db.query(`UPDATE timetable t JOIN students s ON t.student_id = s.id SET t.start_time = '13:00:00', t.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND (t.start_time LIKE '%01:00%' OR t.start_time LIKE '%1:00%')`);
+        await db.query(`UPDATE faculty_schedules fs JOIN students s ON fs.student_id = s.id SET fs.start_time = '13:00:00', fs.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND (fs.start_time LIKE '%01:00%' OR fs.start_time LIKE '%1:00%')`);
+        await db.query(`UPDATE faculty_sessions fs JOIN timetable t ON fs.timetable_id = t.id JOIN students s ON t.student_id = s.id SET fs.start_time = '13:00:00', fs.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND (fs.start_time LIKE '%01:00%' OR fs.start_time LIKE '%1:00%')`);
+        await db.query(`UPDATE faculty_sessions fs JOIN session_attendance sa ON fs.id = sa.session_id JOIN students s ON sa.student_id = s.id SET fs.start_time = '13:00:00', fs.end_time = '14:00:00' WHERE s.name LIKE '%zenha%' AND (fs.start_time LIKE '%01:00%' OR fs.start_time LIKE '%1:00%')`);
 
         console.log("Times fixed successfully!");
 
