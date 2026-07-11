c2703bb35591e6768e12666ffe47545af09d2437
bibin <bibinthankachan29@gmail.com>
2026-07-11 16:57:46 +0530

    Changed Demo Schedules sort order to DESC

--- backend/controllers/aoeController.js
+++ backend/controllers/aoeController.js
@@ -1593,7 +1593,7 @@ const getDemoSchedules = async (req, res) => {
             params.push(searchParam, searchParam, searchParam);
         }
 
-        query += ' ORDER BY d.id ASC';
+        query += ' ORDER BY d.id DESC';
 
         if (page) {
             const pageNum = parseInt(page) || 1;
